e7e482ef28fb05c54970c5d77d2851c92da5e466
MaggieDaisy <missgosia84@gmail.com>
2021-03-11 00:09:19 +0000

    Add jQuery hide and show effect on trips buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,23 @@
-// Show and/or hide active class on toggle icon 
-
+// jQuery show and hide active link on toggle icon 
+// Credit - Code inspiration borrowed from "Responsive Navigation Menu Bar with HTML CSS & Javascript | CSS Flexbox" Tutorial by CodingNepal- https://www.youtube.com/watch?v=mbDNrvKLAGM
 $(document).ready(function(){
 $('.nav-btn').click(function(){
     $('.nav-link').toggleClass("show");
     $('ul li').toggleClass("hide");
 });
+});
+
+
+// jQuery show and hide effects for buttons at Trips section
+// Credit - Code inspiration borrowed from content of "JQuery Effects - Challenge 3" by Code Institute course- https://courses.codeinstitute.net/courses/course-v1:CodeInstitute+IFD101+2017_T3/courseware/b99394c03fb342ed96e82caa5adef426/cb6592e199ef45d5a3db4cf83691b70f/?child=first
+$("#button-trip-1").click(function(){
+    $("#des-1").slideToggle(500);
+});
+
+$("#button-trip-2").click(function(){
+    $("#des-2").slideToggle(500);
+});
+
+$("#button-trip-3").click(function(){
+    $("#des-3").slideToggle(500);
 });
