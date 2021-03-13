be97d92ed1e278e4c93bfdd5b6233886cc48999d
MaggieDaisy <missgosia84@gmail.com>
2021-03-13 16:55:14 +0000

    Add live site link to readme file, add reset function to sendemail.js, move script to bottom page

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -1,16 +1,22 @@
+// Function borrowed from Course Project Walkthrough about Rosie - Putting it all together, Sending Emails Using EmailJS
 function sendMail(responsiveForm) {
-emailjs.send("service_4f9lyhb", "sardinia", {
-    "from_name": responsiveForm.name.value,
-    "from_email": responsiveForm.email.value,
-    "message": responsiveForm.textarea.value
-})
+    emailjs
+        .send("service_4f9lyhb", "sardinia", {
+            from_name: responsiveForm.name.value,
+            from_email: responsiveForm.email.value,
+            message: responsiveForm.textarea.value,
+        })
+        .then(
+            function (response) {
+                console.log("Your email has been sent with success", response);
+            },
+            function (error) {
+                console.log("Sending the email has failed", error);
+            }
+        );
+    // Clear form from inputs after subbmision
+    document.getElementById("responsiveForm").reset();
 
-.then (
-    function(response) {
-console.log("SUCCESS", response);
-    },
-    function(error) {
-        console.log("FAILED", error);
-    });
-return false;
+    //Prevents the page from refreshing when submiting form
+    return false;
 }
