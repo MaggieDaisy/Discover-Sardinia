28d1b7a59abb7e73751e633b571f5e658ecdd213
MaggieDaisy <missgosia84@gmail.com>
2021-03-16 02:02:04 +0000

    Add variables and hide effect for trips button, add values lat and lng to map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,91 +1,25 @@
-/*
-// Initialize and add the map
-function initMap() {
-  // Add general location of island
-  const sardinia = { lat: 40.1209, lng: 9.0129 };
-  // Center map at Mediterranean area
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 6,
-    center: sardinia,
-  });
-
-  const features = [
-    {
-      position: new google.maps.LatLng(38.8841, 8.8640),
-      title: "Su Giudeu Beach"
-    },
-    {
-      position: new google.maps.LatLng(40.1085, 9.6893),
-      title: "Cala Goloritze Beach"
-    },
-    {
-      position: new google.maps.LatLng(40.8947, 9.6146),
-      title: "Porto Istana Beach"
-    },
-    {
-      position: new google.maps.LatLng(41.0891685, 9.565959),
-      title: "Spiaggia del Principe"
-    },
-    {
-      position: new google.maps.LatLng(39.1161, 9.5194),
-      title: "Spiaggia di Porto Giunco"
-    },
-    {
-      position: new google.maps.LatLng(40.9655, 8.2095),
-      title: "Spiaggia La Pelosa"
-    },
-    {
-      position: new google.maps.LatLng(38.8946, 8.8131),
-      title: "Tuerredda Beach"
-    },
-    {
-      position: new google.maps.LatLng(39.8760, 8.6109),
-      title: "Santa Giusta Beach"
-    },
-    {
-      position: new google.maps.LatLng(39.1325, 9.5571),
-      title: "Punta Molentis Beach"
-    },
-    {
-      position: new google.maps.LatLng(40.8347, 9.6858),
-      title: "Cala Brandinchi"
-    },
-  ];
-  //  Markers
-  for (let i = 0; i < features.length; i++) {
-    const marker = new google.maps.Marker({
-      position: features[i].position,
-      map: map,
-    });
-  }
-}
-*/
-
-
 // Initialize and add the map and create complex markers to indicate attractions of island
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 6,
     center: { lat: 40.1209, lng: 9.0129 },
   });
-  // Create an array of alphabetical characters used to label the markers
-  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   // Add markers locations of attractions to the map
-  const markers = locations.map((location, i) => {
+  const markers = attractions.map((attraction, i) => {
     return new google.maps.Marker({
-      position: location,
-      label: labels[i % labels.length],
+      position: attraction,
+      map: map
     });
   });
-  // Add a marker clusterer to manage the markers
+  // Add a marker clusterer to show the markers
   new MarkerClusterer(map, markers, {
     imagePath:
       "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
   });
 }
 // Create an object to display locations on the map, add latitude and longitude of attractions 
-const locations = [
-  { lat: 38.8841, lng: 8.8640 }, // "Su Giudeu Beach"
+const attractions = [
+  { lat: 38.8841, lng: 8.8640, title: "Su Giudeu Beach" }, // "
   { lat: 40.1085, lng: 9.6893 }, // "Cala Goloritze Beach"
   { lat: 40.8947, lng: 9.6146 }, // "Porto Istana Beach"
   { lat: 41.0891, lng: 9.5659 }, // "Spiaggia del Principe"
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,27 @@
 // Credit - Code inspiration borrowed from "Responsive Navigation Menu Bar with HTML CSS & Javascript | CSS Flexbox" Tutorial by CodingNepal- https://www.youtube.com/watch?v=mbDNrvKLAGM
 $(document).ready(function(){
 $('.nav-btn').click(function(){
-    $('.nav-link').toggleClass("show");
-    $('ul li').toggleClass("hide");
+    $('.nav-link').toggleClass("hide");
+    $('ul li').toggleClass("show");
 });
 });
-
 
 // jQuery show and hide effects for buttons at Trips section
 // Credit - Code inspiration borrowed from content of "JQuery Effects - Challenge 3" by Code Institute course- https://courses.codeinstitute.net/courses/course-v1:CodeInstitute+IFD101+2017_T3/courseware/b99394c03fb342ed96e82caa5adef426/cb6592e199ef45d5a3db4cf83691b70f/?child=first
+var tripElement = $("#des-1");
+tripElement.hide()
 $("#button-trip-1").click(function(){
     $("#des-1").slideToggle(500);
 });
 
+var tripElement = $("#des-2");
+tripElement.hide()
 $("#button-trip-2").click(function(){
     $("#des-2").slideToggle(500);
 });
 
+var tripElement = $("#des-3");
+tripElement.hide()
 $("#button-trip-3").click(function(){
     $("#des-3").slideToggle(500);
 });
