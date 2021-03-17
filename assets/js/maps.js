9e6a1a9d934bf5dde5d1279f4848a3c39c5483c7
MaggieDaisy <missgosia84@gmail.com>
2021-03-17 20:08:40 +0000

    Add comments, clean typos, beautify code, fix function success and error in emailjs

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,202 +1,196 @@
-
 // Credit - Code inspiration borrowed from "Google Maps JavaScript API Tutorial" by Traversy Media - https://www.youtube.com/watch?v=Zxf1mnP5zcw
-// Initialize and add the map and create complex markers to indicate attractions of island
+
+// Initialize and add the map and create complex markers to indicate attractions of the island
 function initMap() {
     //Create map options to add general map of area, add zoom and center
-    var options = {
+    const options = {
         zoom: 6,
         center: { lat: 40.1209, lng: 9.0129 },
     };
     //Create the map and include options
-    var map = new google.maps.Map(document.getElementById("map"), options);
+    const map = new google.maps.Map(document.getElementById("map"), options);
 
-    //Create event listener for click on map to make function active
-    google.maps.event.addListener(map, "click", function (event) {
-        //Include marker for extracting coordinates (latitude and longitude of attractions)
-        addMarker({ coords: event.latLng });
-    });
-
-    //Create array of markers for attractions, store coordinates data and content to display lokation and title of place
-    var markers = [
+    //Create an array of markers for attractions, store coordinates data and content to display location and title of the place
+    const markers = [
         {
-            coords: { lat: 40.1085, lng: 9.6893 },
+            coordinates: { lat: 40.1085, lng: 9.6893 },
             content: "<p>Top 10 beaches: Cala Goloritze Beach<p>",
         },
         {
-            coords: { lat: 40.8947, lng: 9.6146 },
+            coordinates: { lat: 40.8947, lng: 9.6146 },
             content: "<p>Top 10 beaches: Porto Istana Beach<p>",
         },
         {
-            coords: { lat: 41.0891, lng: 9.5659 },
+            coordinates: { lat: 41.0891, lng: 9.5659 },
             content: "<p>Top 10 beaches: Spiaggia del Principe<p>",
         },
         {
-            coords: { lat: 39.1161, lng: 9.5194 },
+            coordinates: { lat: 39.1161, lng: 9.5194 },
             content: "<p>Top 10 beaches: Spiaggia di Porto Giunco<p>",
         },
         {
-            coords: { lat: 38.8841, lng: 8.864 },
+            coordinates: { lat: 38.8841, lng: 8.864 },
             content: "<p>Top 10 beaches: Su Giudeu Beach <p>",
         },
         {
-            coords: { lat: 40.9655, lng: 8.2095 },
+            coordinates: { lat: 40.9655, lng: 8.2095 },
             content: "<p>Top 10 beaches: Spiaggia La Pelosa<p>",
         },
         {
-            coords: { lat: 38.8946, lng: 8.8131 },
+            coordinates: { lat: 38.8946, lng: 8.8131 },
             content: "<p>Top 10 beaches: Tuerredda Beach<p>",
         },
         {
-            coords: { lat: 39.876, lng: 8.6109 },
+            coordinates: { lat: 39.876, lng: 8.6109 },
             content: "<p>Top 10 beaches: Santa Giusta Beach<p>",
         },
         {
-            coords: { lat: 40.8347, lng: 9.6858 },
+            coordinates: { lat: 40.8347, lng: 9.6858 },
             content: "<p>Top 10 beaches: Cala Brandinchi<p>",
         },
         {
-            coords: { lat: 39.1325, lng: 9.5571 },
+            coordinates: { lat: 39.1325, lng: 9.5571 },
             content: "<p>Top 10 beaches: Punta Molentis Beach<p>",
         },
 
         {
-            coords: { lat: 39.1296, lng: 9.4653 },
+            coordinates: { lat: 39.1296, lng: 9.4653 },
             content: "<p>Top 10 hotels: Falkensteiner Resort Capo Boi<p>",
         },
         {
-            coords: { lat: 40.5505, lng: 8.3181 },
+            coordinates: { lat: 40.5505, lng: 8.3181 },
             content: "<p>Top 10 hotels: Villa Las Tronas Hotel & Spa<p>",
         },
         {
-            coords: { lat: 41.1514, lng: 9.5274 },
+            coordinates: { lat: 41.1514, lng: 9.5274 },
             content: "<p>Top 10 hotels: Colonna Resort<p>",
         },
         {
-            coords: { lat: 40.9291, lng: 8.802 },
+            coordinates: { lat: 40.9291, lng: 8.802 },
             content: "<p>Top 10 hotels: Casteldoria Beach Resort<p>",
         },
         {
-            coords: { lat: 38.8962, lng: 8.8682 },
+            coordinates: { lat: 38.8962, lng: 8.8682 },
             content: "<p>Top 10 hotels: Hotel Chia Laguna<p>",
         },
         {
-            coords: { lat: 39.013, lng: 9.0014 },
-            content: "<p>Top 10 beaches: Forte Village Resort<p>",
+            coordinates: { lat: 39.013, lng: 9.0014 },
+            content: "<p>Top 10 hotels: Forte Village Resort<p>",
         },
         {
-            coords: { lat: 40.8219, lng: 9.6762 },
+            coordinates: { lat: 40.8219, lng: 9.6762 },
             content: "<p>Top 10 hotels: Paradise Resort & Spa<p>",
         },
         {
-            coords: { lat: 41.1693, lng: 9.4187 },
+            coordinates: { lat: 41.1693, lng: 9.4187 },
             content: "<p>Top 10 hotels: Hotel Capo di Orso Thalasso & SPA<p>",
         },
         {
-            coords: { lat: 40.3726, lng: 9.7193 },
+            coordinates: { lat: 40.3726, lng: 9.7193 },
             content: "<p>Top 10 hotels: Club Hotel Marina Beach<p>",
         },
         {
-            coords: { lat: 39.1132, lng: 9.4997 },
+            coordinates: { lat: 39.1132, lng: 9.4997 },
             content: "<p>Top 10 hotels: Hotel Santo Stefano Clubviaggi<p>",
         },
         {
-            coords: { lat: 39.2238, lng: 9.1217 },
+            coordinates: { lat: 39.2238, lng: 9.1217 },
             content: "<p>Top 10 attractions: Cagliari - capital<p>",
         },
         {
-            coords: { lat: 39.0374, lng: 8.4184 },
+            coordinates: { lat: 39.0374, lng: 8.4184 },
             content: "<p>Top 10 attractions: Sant Antioco Island<p>",
         },
         {
-            coords: { lat: 39.8733, lng: 8.4397 },
+            coordinates: { lat: 39.8733, lng: 8.4397 },
             content: "<p>Top 10 attractions: Tharros<p>",
         },
         {
-            coords: { lat: 41.2166, lng: 9.4047 },
+            coordinates: { lat: 41.2166, lng: 9.4047 },
             content: "<p>Top 10 attractions: La Madallena<p>",
         },
         {
-            coords: { lat: 39.9062, lng: 8.5884 },
+            coordinates: { lat: 39.9062, lng: 8.5884 },
             content: "<p>Top 10 attractions: Oristano<p>",
         },
         {
-            coords: { lat: 40.9137, lng: 8.7086 },
+            coordinates: { lat: 40.9137, lng: 8.7086 },
             content: "<p>Top 10 attractions: Castelsardo<p>",
         },
         {
-            coords: { lat: 40.3, lng: 9.6833 },
+            coordinates: { lat: 40.3, lng: 9.6833 },
             content: "<p>Top 10 attractions: Gulf of Orosei<p>",
         },
         {
-            coords: { lat: 40.2055, lng: 9.3484 },
+            coordinates: { lat: 40.2055, lng: 9.3484 },
             content: "<p>Top 10 attractions: Orgosolo<p>",
         },
         {
-            coords: { lat: 40.9236, lng: 9.4964 },
+            coordinates: { lat: 40.9236, lng: 9.4964 },
             content: "<p>Top 10 attractions: Costa Smeralda<p>",
         },
         {
-            coords: { lat: 40.1852, lng: 9.5014 },
+            coordinates: { lat: 40.1852, lng: 9.5014 },
             content: "<p>Top 10 attractions: Gorropu Gorge<p>",
         },
         {
-            coords: { lat: 40.9243, lng: 9.4992 },
+            coordinates: { lat: 40.9243, lng: 9.4992 },
             content: "<p>Top 10 restaurants: Dolceacqua - Cucina Ligure & Sarda<p>",
         },
         {
-            coords: { lat: 41.1156, lng: 9.5279 },
+            coordinates: { lat: 41.1156, lng: 9.5279 },
             content: "<p>Top 10 restaurants: I Frati Rossi<p>",
         },
         {
-            coords: { lat: 40.5568, lng: 8.314 },
+            coordinates: { lat: 40.5568, lng: 8.314 },
             content: "<p>Top 10 restaurants: Ristorante Al Tuguri<p>",
         },
         {
-            coords: { lat: 40.924, lng: 9.5034 },
+            coordinates: { lat: 40.924, lng: 9.5034 },
             content: "<p>Top 10 restaurants: Antica Trattoria<p>",
         },
         {
-            coords: { lat: 39.2133, lng: 9.1158 },
+            coordinates: { lat: 39.2133, lng: 9.1158 },
             content: "<p>Top 10 restaurants: Ristorante Dal Corsaro<p>",
         },
         {
-            coords: { lat: 41.0518, lng: 9.446 },
+            coordinates: { lat: 41.0518, lng: 9.446 },
             content: "<p>Top 10 restaurants: Ristorante Il Fuoco Sacro<p>",
         },
         {
-            coords: { lat: 40.6156, lng: 8.288 },
+            coordinates: { lat: 40.6156, lng: 8.288 },
             content: "<p>Top 10 restaurants: Agriturismo Barbagia<p>",
         },
         {
-            coords: { lat: 39.2867, lng: 9.498 },
+            coordinates: { lat: 39.2867, lng: 9.498 },
             content: "<p>Top 10 restaurants: Agriturismo la Sorgente<p>",
         },
         {
-            coords: { lat: 39.3181, lng: 9.532 },
+            coordinates: { lat: 39.3181, lng: 9.532 },
             content: "<p>Top 10 restaurants: Agriturismo Il Nuraghe<p>",
         },
         {
-            coords: { lat: 40.6247, lng: 9.6974 },
+            coordinates: { lat: 40.6247, lng: 9.6974 },
             content: "<p>Top 10 restaurants: Agriturismo su Vrau<p>",
         },
     ];
 
     //Loop through the markers
-    for (var i = 0; i < markers.length; i++) {
+    for (let i = 0; i < markers.length; i++) {
         addMarker(markers[i]);
     }
-    //Add function and check content, add event listener for display content
-    function addMarker(props) {
-        var marker = new google.maps.Marker({
-            position: props.coords,
+    //Add function and check content
+    function addMarker(properties) {
+        let marker = new google.maps.Marker({
+            position: properties.coordinates,
             map: map,
         });
 
-        if (props.content) {
-            var infoWindow = new google.maps.InfoWindow({
-                content: props.content,
+        if (properties.content) {
+            let infoWindow = new google.maps.InfoWindow({
+                content: properties.content,
             });
-
+            //Create event listener for click on map to make function active and dispaly content
             marker.addListener("click", function () {
                 infoWindow.open(map, marker);
             });
--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -1,22 +1,26 @@
-// Function borrowed from Course Project Walkthrough about Rosie - Putting it all together, Sending Emails Using EmailJS
+// Credit - code inspiration borrowed from Course Project Walkthrough about Rosie - Putting it all together, Sending Emails Using EmailJS
+
+// Create function send email, add send method which contains three arguments
+// (service ID, template ID, and an object with three parameters which matches template EmailJS)
 function sendMail(responsiveForm) {
     emailjs
-        .send("service_4f9lyhb", "sardinia", {
+        .send("gmail", "sardinia", {
             from_name: responsiveForm.name.value,
             from_email: responsiveForm.email.value,
             message: responsiveForm.textarea.value,
         })
+        // Add then method and insert two functions, first will return a response that sending email was a success, second instead shows an error
         .then(
-            function (response) {
-                console.log("Your email has been sent with success", response);
+            function (success) {
+                alert("Thank you for contacting us, your email has been sent with success", success);
             },
             function (error) {
-                console.log("Sending the email has failed", error);
+                alert("We are sorry, but sending the email has failed", error);
             }
         );
-    // Clear form from inputs after subbmision
+    // Add event listener to clear form from inputs after subbmision
     document.getElementById("responsiveForm").reset();
 
-    //Prevents the page from refreshing when submiting form
+    //Add return statement which prevents the page from refreshing when submitting form
     return false;
 }
